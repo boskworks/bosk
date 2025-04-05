c3a92792efd29909e0407e7e5c4558f1b38827d6
Patrick Doyle <p.r.doyle@gmail.com>
2025-04-05 16:57:06 -0400

    Remove compilerArg "-parameters"

--- bosk-spring-boot-3/src/main/java/works/bosk/spring/boot/WebProperties.java
+++ bosk-spring-boot-3/src/main/java/works/bosk/spring/boot/WebProperties.java
@@ -1,13 +1,9 @@
 package works.bosk.spring.boot;
 
-import lombok.Value;
-import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "bosk.web")
-@Value
-@Accessors(fluent = false)
-public class WebProperties {
-	Boolean readContext;
-	String maintenancePath;
-}
+public record WebProperties(
+	Boolean readContext,
+	String maintenancePath
+) {}
