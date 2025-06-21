b1b40713de6fd1f4fa16e294ba073dd31169b8f1
Patrick Doyle <p.r.doyle@gmail.com>
2025-06-21 13:37:58 -0400

    Improve error handling in HookScanner

--- bosk-core/src/main/java/works/bosk/HookScanner.java
+++ bosk-core/src/main/java/works/bosk/HookScanner.java
@@ -4,6 +4,7 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -33,56 +34,17 @@ class HookScanner {
 					continue;
 				}
 				if (isStatic(method.getModifiers())) {
-					throw new IllegalArgumentException("Hook method cannot be static: " + method);
+					throw new InvalidTypeException("Hook method cannot be static: " + method);
 				} else if (isPrivate(method.getModifiers())) {
-					throw new IllegalArgumentException("Hook method cannot be private: " + method);
-				}
-				Path path = Path.parseParameterized(hookAnnotation.value());
-
-				Reference<?> plainRef = bosk.rootReference().then(Object.class, path);
-				// Now substitute one of the handy Reference subtypes where possible
-				Reference<?> scope;
-				if (Catalog.class.isAssignableFrom(plainRef.targetClass())) {
-					scope = bosk.rootReference().thenCatalog(Entity.class, path);
-				} else if (Listing.class.isAssignableFrom(plainRef.targetClass())) {
-					scope = bosk.rootReference().thenListing(Entity.class, path);
-				} else if (SideTable.class.isAssignableFrom(plainRef.targetClass())) {
-					scope = bosk.rootReference().thenSideTable(Entity.class, Object.class, path);
-				} else {
-					scope = plainRef;
+					throw new InvalidTypeException("Hook method cannot be private: " + method);
 				}
 
-				List<Function<Reference<?>, Object>> argumentFunctions = new ArrayList<>(method.getParameterCount());
-				argumentFunctions.add(ref -> receiverObject); // The "this" pointer
-				for (Parameter p : method.getParameters()) {
-					if (Reference.class.isAssignableFrom(p.getType())) {
-						if (ReferenceUtils.parameterType(p.getParameterizedType(), Reference.class, 0).equals(scope.targetType())) {
-							argumentFunctions.add(ref -> ref);
-						} else {
-							throw new IllegalArgumentException("Expected reference to " + scope.targetType() + ": " + method.getName() + " parameter " + p);
-						}
-					} else if (p.getType().isAssignableFrom(BindingEnvironment.class)) {
-						argumentFunctions.add(ref -> scope.parametersFrom(ref.path()));
-					} else {
-						throw new IllegalArgumentException("Unsupported parameter type " + p.getType() + ": " + method.getName() + " parameter " + p);
-					}
-				}
-				MethodHandle hook;
 				try {
-					hook = MethodHandles.lookup().unreflect(method);
-				} catch (IllegalAccessException e) {
-					throw new IllegalArgumentException(e);
+					registerOneHookMethod(receiverObject, bosk, method, Path.parseParameterized(hookAnnotation.value()));
+					hookCounter++;
+				} catch (InvalidTypeException e) {
+					throw new InvalidTypeException("Unable to register hook method " + receiverClass.getSimpleName() + "." + method.getName() + ": " + e.getMessage(), e);
 				}
-				bosk.registerHook(method.getName(), scope, ref -> {
-					try {
-						List<Object> arguments = new ArrayList<>(argumentFunctions.size());
-						argumentFunctions.forEach(f -> arguments.add(f.apply(ref)));
-						hook.invokeWithArguments(arguments);
-					} catch (Throwable e) {
-						throw new IllegalStateException("Unable to call hook \"" + method.getName() + "\"", e);
-					}
-				});
-				hookCounter++;
 			}
 		}
 		if (hookCounter == 0) {
@@ -90,6 +52,55 @@ class HookScanner {
 		} else {
 			LOGGER.info("Registered {} hook{} in {}", hookCounter, (hookCounter >= 2) ? "s" : "", receiverObject.getClass().getSimpleName());
 		}
+	}
+
+	private static <T> void registerOneHookMethod(T receiverObject, Bosk<?> bosk, Method method, Path path) throws InvalidTypeException {
+		Reference<?> plainRef = bosk.rootReference().then(Object.class, path);
+
+		// Now substitute one of the handy Reference subtypes where possible
+		Reference<?> scope;
+		if (Catalog.class.isAssignableFrom(plainRef.targetClass())) {
+			scope = bosk.rootReference().thenCatalog(Entity.class, path);
+		} else if (Listing.class.isAssignableFrom(plainRef.targetClass())) {
+			scope = bosk.rootReference().thenListing(Entity.class, path);
+		} else if (SideTable.class.isAssignableFrom(plainRef.targetClass())) {
+			scope = bosk.rootReference().thenSideTable(Entity.class, Object.class, path);
+		} else {
+			scope = plainRef;
+		}
+
+		List<Function<Reference<?>, Object>> argumentFunctions = new ArrayList<>(method.getParameterCount());
+		argumentFunctions.add(ref -> receiverObject); // The "this" pointer
+		for (Parameter p : method.getParameters()) {
+			if (Reference.class.isAssignableFrom(p.getType())) {
+				Type referenceType = ReferenceUtils.parameterType(p.getParameterizedType(), Reference.class, 0);
+				if (referenceType.equals(scope.targetType())) {
+					argumentFunctions.add(ref -> ref);
+				} else {
+					throw new InvalidTypeException("Parameter \"" + p.getName() + "\" should be a reference to " + scope.targetType() + ", not " + referenceType);
+				}
+			} else if (p.getType().isAssignableFrom(BindingEnvironment.class)) {
+				argumentFunctions.add(ref -> scope.parametersFrom(ref.path()));
+			} else {
+				throw new InvalidTypeException("Unsupported parameter type: " + p);
+			}
+		}
+
+		MethodHandle hook;
+		try {
+			hook = MethodHandles.lookup().unreflect(method);
+		} catch (IllegalAccessException e) {
+			throw new IllegalArgumentException(e);
+		}
+		bosk.registerHook(method.getName(), scope, ref -> {
+			try {
+				List<Object> arguments = new ArrayList<>(argumentFunctions.size());
+				argumentFunctions.forEach(f -> arguments.add(f.apply(ref)));
+				hook.invokeWithArguments(arguments);
+			} catch (Throwable e) {
+				throw new IllegalStateException("Unable to call hook \"" + method.getName() + "\"", e);
+			}
+		});
 	}
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HookScanner.class);
