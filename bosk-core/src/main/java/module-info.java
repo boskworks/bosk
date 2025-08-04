1be7278e3395248689db46af2a8c3132e127d9c7
Patrick Doyle <p.r.doyle@gmail.com>
2025-08-04 12:17:16 -0400

    works.bosk.sql module

--- bosk-core/src/main/java/module-info.java
+++ bosk-core/src/main/java/module-info.java
@@ -12,5 +12,5 @@ module works.bosk.core {
 	exports works.bosk.exceptions;
 	exports works.bosk.bytecode to works.bosk.jackson;
 	exports works.bosk.util to works.bosk.jackson, works.bosk.mongo;
-	exports works.bosk.logging to works.bosk.logback, works.bosk.mongo; // May be ok to export this, but give it a think first
+	exports works.bosk.logging to works.bosk.logback, works.bosk.mongo, works.bosk.sql; // May be ok to export this, but give it a think first
 }
