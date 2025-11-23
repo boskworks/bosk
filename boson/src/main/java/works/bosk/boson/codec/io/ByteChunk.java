946588c3f9e2ec390bd115d1a5831159e045ed71
Patrick Doyle <p.r.doyle@gmail.com>
2025-11-23 11:33:07 -0500

    Javadocs for ByteChunk

--- boson/src/main/java/works/bosk/boson/codec/io/ByteChunk.java
+++ boson/src/main/java/works/bosk/boson/codec/io/ByteChunk.java
@@ -1,7 +1,12 @@
 package works.bosk.boson.codec.io;
 
 /**
- * @param start the index of the first byte in the chunk containing data
+ * @param bytes the byte array containing the chunk data.
+ *              May be modified by {@link ByteChunkJsonReader}.
+ * @param start the index of the first byte in the chunk containing data.
+ *              Unless you're implementing carryover logic,
+ *              or otherwise know exactly what you're doing,
+ *              this should be at least {@link ByteChunkJsonReader#CARRYOVER_BYTES CARRYOVER_BYTES}.
  * @param stop  the index one past the last byte in the chunk containing data
  */
 public record ByteChunk(
