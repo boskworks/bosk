fe8472284886727d7d76c4e9299e90874e75aaf1
Patrick Doyle <p.r.doyle@gmail.com>
2025-12-20 11:00:46 -0500

    ChangeListener javadocs

--- bosk-mongo/src/main/java/works/bosk/drivers/mongo/internal/ChangeListener.java
+++ bosk-mongo/src/main/java/works/bosk/drivers/mongo/internal/ChangeListener.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 import org.bson.BsonDocument;
 
+/**
+ * Used by {@link ChangeReceiver} to delegate handling of events to other components.
+ * <p>
+ * Meeting the ordering guarantees of {@link works.bosk.BoskDriver BoskDriver}
+ * is simplified tremendously if all change events are processed by the same thread,
+ * and we use the {@link ChangeReceiver} thread for that purpose,
+ * yet we don't want to cram all the event handling logic
+ * into {@code ChangeReceiver} itself.
+ */
 interface ChangeListener {
 	void onConnectionSucceeded() throws
 		UnrecognizedFormatException,
