d09b7c384a3434e609b4f36618e48f6f26d2bc3a
Patrick Doyle <p.r.doyle@gmail.com>
2026-03-07 11:45:00 -0500

    Modify generated files

--- example-hello/src/main/java/works/bosk/hello/HelloApplication.java
+++ example-hello/src/main/java/works/bosk/hello/HelloApplication.java
@@ -2,12 +2,18 @@ package works.bosk.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import works.bosk.logback.BoskLogFilter;
 
 @SpringBootApplication
 public class HelloApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
+	@Bean
+	BoskLogFilter.LogController logController() {
+		return new BoskLogFilter.LogController();
+	}
 }
